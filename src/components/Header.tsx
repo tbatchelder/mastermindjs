4327cc24955574eec6a66ea10f4c00b47ca61407
tbatchelder <pegacorn@optonline.net>
2026-03-19 20:13:09 -0400

    Removed unneed react calls

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import logo from "../assets/beam-logo.svg";
 
 interface HeaderProps {
--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { GameSettings, LevelStats } from "../types";
 import {
   SLOT_MIN,
